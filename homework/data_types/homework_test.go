01999ac82272614e1b27b5010477d269816c81b8
v0xa <v0xa49@icloud.com>
2026-05-24 19:03:39 +0300

    implement ToLittleEndian

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
+func ToLittleEndian[T ~uint16 | ~uint32 | ~uint64](number T) T {
+	size := (uint)(unsafe.Sizeof(number))
+	bytePointer := (unsafe.Pointer(&number))
+	for i := uint(0); i != size/2; i++ {
+		x := (*uint8)(unsafe.Add(bytePointer, i))
+		y := (*uint8)(unsafe.Add(bytePointer, size-i-1))
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+		*x, *y = *y, *x
+	}
+	return number
 }
 
 func TestСonversion(t *testing.T) {
@@ -36,6 +44,88 @@ func TestСonversion(t *testing.T) {
 		"test case #5": {
 			number: 0x01020304,
 			result: 0x04030201,
+		},
+		"test case #6": {
+			number: 0x12345678,
+			result: 0x78563412,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversion16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #5": {
+			number: 0x0102,
+			result: 0x0201,
+		},
+		"test case #6": {
+			number: 0x1234,
+			result: 0x3412,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversion64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF0000FFFF,
+			result: 0xFFFF0000FFFF0000,
+		},
+		"test case #5": {
+			number: 0x0102030401020304,
+			result: 0x0403020104030201,
+		},
+		"test case #6": {
+			number: 0x1234567812345678,
+			result: 0x7856341278563412,
 		},
 	}
 
